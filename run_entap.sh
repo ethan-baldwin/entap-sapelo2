7e3bb65f24201e117bcdd7522556d602ac3fcd1e
Ethan Baldwin <70599620+ethan-baldwin@users.noreply.github.com>
2025-10-29 14:29:27 -0400

    Update EnTAP command and remove SLURM output options

--- run_entap.sh
+++ run_entap.sh
@@ -5,10 +5,7 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=50gb
 #SBATCH --time=72:00:00
-#SBATCH --mail-type=END,FAIL
-#SBATCH --output=/scratch/eab77806/logs/%x_%j.out
-#SBATCH --error=/scratch/eab77806/logs/%x_%j.err
 
 ml EnTAP/2.2.0-GCCcore-11.3.0
 
-EnTAP --run -i {peptides.fa} -t $SLURM_CPUS_PER_TASK --entap-ini {path_to_config.ini} --run-ini {path_to_run.ini}
+EnTAP --run -i path/to/your/peptides.fa -t $SLURM_CPUS_PER_TASK --out-dir path/to/your/outdir --entap-ini entap_config.ini --run-ini entap_run.ini 
